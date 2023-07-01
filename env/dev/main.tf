1dc3badf9c972ab7be45e3348c030db931c6afbe
shubham-likhar <shubham.likhar@opstree.com>
2023-07-01 10:32:43 +0530

    load-balancer-code

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,16 +1,29 @@
 
 
 
-module "vpc" {
+# module "vpc" {
+#   env = "dev"
+#   appname = "web-app"
+#   source         = "../../modules/vpc"
+#   vpc_cidr_block = "10.0.0.0/16"
+#   public_subnet  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   private_subnet = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+#   tags = {
+#     Name = "cloudblitz"
+#     owner = "shubham likhar"
+#   }
+
+# }
+
+
+module "alb" {
+  source = "../../modules/load-balancer"
   env = "dev"
   appname = "web-app"
-  source         = "../../modules/vpc"
-  vpc_cidr_block = "10.0.0.0/16"
-  public_subnet  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnet = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  tags = {
-    Name = "cloudblitz"
-    owner = "shubham likhar"
-  }
+  internal = true
+  load_balancer_type = "application"
+  
 
+
+  
 }
